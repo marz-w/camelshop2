5d10ad322633cda71329d9c9fbf5c097e7cf9766
Ann Chemutai <annchemu@gmail.com>
2024-06-20 13:29:47 +0300

    modified home and products pages

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -35,27 +35,87 @@
 </script>
 
 <template>
-  <h1>My Candy Shop</h1>
-    <div class="center">
-        <img v-bind:src="image" width="500" height="300">
-    </div>
-    <div class="center">
-        <a :href="url">Vue JS Practice</a>
-        <h3>Milk, white, dark </h3>
-        <h5> A wide variety of fine snacks, sweets and chocoloates perfect for all occassions</h5>
-    </div>
-    <div class="center">
-        <ul>
-            <li :key="item" v-for="item in stockDetails">
-              <div>
-                <img v-bind:src="item.itemImage" width="200" height="150">
-              </div>
-              <div>
-                  <span>{{ item.itemName }}</span> &nbsp; &nbsp;
-                  <span> <button style="background-color: blueviolet; color: white; border: blueviolet;">+</button></span>
-              </div>
-              <br/>
-            </li>
-        </ul> 
-    </div>
-    </template>
+<v-card
+    class="mx-auto"
+    width="1000"
+  >
+
+    <v-carousel>
+        <v-carousel-item
+            src="../images/vinicius-amnx-amano-dbOV1qSiL-c-unsplash.jpg"
+            cover
+        ></v-carousel-item>
+
+  <v-carousel-item
+    src="../images/customerbox-aXq1oCCjlVM-unsplash.jpg"
+    cover
+  ></v-carousel-item>
+
+  <v-carousel-item
+    src="../images/jamie-albright-AHF_ZktTL6Q-unsplash.jpg"
+    cover
+  ></v-carousel-item>
+</v-carousel>
+  </v-card>
+  <v-spacer></v-spacer>
+  <v-card align="center">
+        <v-card-item>
+          <v-card-title>Welcome to the Camel Candy Shop</v-card-title>
+
+          <v-card-subtitle>This is your one stop shop for candies, sweets, chocolates, and so much more </v-card-subtitle>
+        </v-card-item>
+
+        <v-card-text>
+          Our shop stocks a variety of confectionery for your everyday needs. Indulge your sweet tooth at our online candy shop, where whimsical treats and classic favorites await! Discover a treasure trove of colorful confections, from gourmet chocolates to nostalgic candies that bring back fond memories. Whether you're craving something chewy, crunchy, or melt-in-your-mouth delicious, we have the perfect sugary delight to satisfy every craving. Treat yourself or surprise a loved one with a gift box filled with joyous sweetness delivered right to your doorstep. Welcome to a world of delightful flavors and irresistible treats—browse, click, and let the sweetness begin!
+        </v-card-text>
+      </v-card>
+
+      <v-divider></v-divider>
+      <v-divider></v-divider>
+      <v-divider></v-divider>
+      <h3 class="text-center">What our customers say</h3>
+      <v-divider></v-divider>
+      
+      <v-row>
+    <v-col cols="12" md="4">
+      <v-card
+        subtitle="Ann Chemutai"
+        text="I loved the engagement from the sales person. very informative and quick delivery!"
+        title="Good customer service"
+      ></v-card>
+
+    </v-col>
+
+    <v-col cols="12" md="4">
+      <v-card>
+        <template v-slot:title>
+          Fast delivery!
+        </template>
+
+        <template v-slot:subtitle>
+          Barack Obama
+        </template>
+
+        <template v-slot:text>
+          I recieved my order on time, will definitely be ordering again
+        </template>
+      </v-card>
+
+    </v-col>
+
+    <v-col cols="12" md="4">
+      <v-card>
+        <v-card-item>
+          <v-card-title>Wide variety</v-card-title>
+
+          <v-card-subtitle>Wangari Mathai</v-card-subtitle>
+        </v-card-item>
+
+        <v-card-text>
+          I love the diversity in the options, I got some candy I haven't seen in normal shops.
+        </v-card-text>
+      </v-card>
+
+    </v-col>
+  </v-row>
+</template>
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,9 +1,149 @@
 <template>
-    <h1>Products</h1>
+    <h1>Confectionery</h1>
+    <div>
+        <v-row>
+        <v-col cols="12" md="4"  v-for="product in stockDetails" :key=product>     
+            <v-card
+                class="mx-auto my-12"
+                max-width="374"
+            >
+                <v-img
+                    height="250"
+                    :src="product.itemImage"
+                    cover
+                ></v-img>
+
+                <v-card-item>
+                    <v-card-title>{{product.itemName}}</v-card-title>
+                </v-card-item>
+
+                <v-card-text>
+                    <div class="my-4 text-subtitle-1">{{product.itemPrice}}</div>
+                    <div>{{ product.description }}</div>
+                </v-card-text>
+
+                <v-divider class="mx-4 mb-1"></v-divider>
+                <v-card-actions>
+                    <v-btn
+                        color="deep-purple-lighten-2"
+                        text="Add to Cart"
+                        block
+                        border
+                    ></v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-col>
+        </v-row>
+    </div>
 
 </template>
 <script>
 export default {
-    
+    data(){
+        return{
+            products: [1,2,3,4,5,6,7,8,9,10],
+            stockDetails : [
+            {
+                itemNumber: 1,
+                itemName : "Sweets",
+                itemPrice : 5,
+                itemQuantity : 30,
+                itemImage: '../images/customerbox-aXq1oCCjlVM-unsplash.jpg',
+                description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
+            },
+            {
+                itemNumber: 27466,
+                itemName : "Lollipop",
+                itemPrice : 10,
+                itemQuantity : 4,
+                itemImage: '../images/jamie-albright-AHF_ZktTL6Q-unsplash.jpg',
+                description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
+
+            }
+            ,
+            {
+                itemNumber: 27466,
+                itemName : "Chocolates",
+                itemPrice : 10,
+                itemQuantity : 4,
+                itemImage: '../images/customerbox-aXq1oCCjlVM-unsplash.jpg',
+                description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
+
+            }
+            ,
+            {
+                itemNumber: 27466,
+                itemName : "Crisps",
+                itemPrice : 10,
+                itemQuantity : 4,
+                itemImage: '../images/jamie-albright-AHF_ZktTL6Q-unsplash.jpg',
+                description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
+
+            }
+            ,
+            {
+                itemNumber: 27466,
+                itemName : "Popcorn",
+                itemPrice : 10,
+                itemQuantity : 4,
+                itemImage: '../images/customerbox-aXq1oCCjlVM-unsplash.jpg',
+                description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
+
+            }
+            ,
+            {
+                itemNumber: 27466,
+                itemName : "Bites",
+                itemPrice : 10,
+                itemQuantity : 4,
+                itemImage: '../images/jamie-albright-AHF_ZktTL6Q-unsplash.jpg',
+                description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
+
+            }
+            ,
+            {
+                itemNumber: 27466,
+                itemName : "Dried Fruit",
+                itemPrice : 10,
+                itemQuantity : 4,
+                itemImage: '../images/customerbox-aXq1oCCjlVM-unsplash.jpg',
+                description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
+
+            },
+            {
+                itemNumber: 27466,
+                itemName : "Macaroons",
+                itemPrice : 10,
+                itemQuantity : 4,
+                itemImage: '../images/jamie-albright-AHF_ZktTL6Q-unsplash.jpg',
+                description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
+
+            }
+            ,
+            {
+                itemNumber: 27466,
+                itemName : "Lollipop",
+                itemPrice : 10,
+                itemQuantity : 4,
+                itemImage: '../images/customerbox-aXq1oCCjlVM-unsplash.jpg',
+                description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
+
+            }
+            ,
+            {
+                itemNumber: 27466,
+                itemName : "Lollipop",
+                itemPrice : 10,
+                itemQuantity : 4,
+                itemImage: '../images/jamie-albright-AHF_ZktTL6Q-unsplash.jpg',
+                description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
+
+            }
+        ],
+        }
+    },
+    methods(){
+
+    }
 }
 </script>
